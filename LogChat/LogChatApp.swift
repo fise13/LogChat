e360824c3a9a7783f004a18c43b5e32c2f1b3339
fise13 <victhewise@icloud.com>
2026-01-04 10:57:04 +0500

    Initial Commit

---
+++ LogChat/LogChatApp.swift
@@ -0,0 +1,21 @@
+//
+//  LogChatApp.swift
+//  LogChat
+//
+//  Created by Виктор on 04.01.2026.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct LogChatApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
